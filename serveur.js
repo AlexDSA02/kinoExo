f16fa8afc59792f87cbd0f7b8c2fee031fbb347b
AlexDsa02 <alexandre.dsa02@gmail.com>
2022-03-23 16:56:58 +0100

    fix

--- serveur.js
+++ serveur.js
@@ -3,8 +3,8 @@ const path = require('path')
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/exoRecrute'));
+app.use(express.static(__dirname + '/dist/exo-recrute'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/exoRecrute/index.html'));});
+    '/dist/exo-recrute/index.html'));});
 app.listen(process.env.PORT || 8080);
